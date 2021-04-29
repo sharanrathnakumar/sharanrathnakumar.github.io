acab31168d8750aef27a0da97291d893fc295d35
sharanrathnakumar <sharan_rathnakumar@LAPTOP-D8379M7H.localdomain>
2021-04-29 16:42:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+let c = document.getElementById("my-canvas");
+let ctx = c.getContext("2d");
+
+let loadImage = (src, callback) => {
+  let img = document.createElement("img");
+  img.onload = () => callback("img");
+  img.src = src;
+};
+
+let imagePath = (frameNumber) => {
+  return "/images/idle/" + frameNumber + ".png";
+};
+
+let loadImage = (callback) => {
+  let images = [];
+  let imagesToLoad = 8;
+  [1, 2, 3, 4, 5, 6, 7, 8].forEach((frameNumber) => {
+    let path = imagePath(frameNumber);
+
+    loadImage(oath, (image) => {
+      images[frameNumber - 1] = image;
+      imagesToLoad = imagesToLoad - 1;
+
+      if (imagesToLoad === 0) {
+        callback(images);
+      }
+    });
+  });
+};
+
+let animate = (ctx, images, callback) => {
+  images.forEach((image, index) => {
+    setTimeout(() => {
+      ctx.clearRect(0, 0, 500, 500);
+      ctx.drawImage(image, 0, 0, 500, 500);
+    }, index * 100);
+  });
+
+  setTimeout(callback, images.length * 100);
+};
+loadImages((images) => {
+  animate(ctx, images, () => {
+    console.log("Done!");
+  });
+});
